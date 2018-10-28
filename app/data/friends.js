f214ee82f3b7b0dd837e57599b5efc8ed8039705
Clara Bell <athenaolson@Athenas-MacBook-Pro.local>
2018-10-28 16:23:28 -0400

    commit 3

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Lorelai Gilmore",
+		photo: "http://www.btchflcks.com/wp-content/uploads/2016/12/Gilmore-Girls-6.png",
+		scores: [2, 5, 5, 1, 3, 1, 1, 5, 2, 5]
+	},
+	{
+		name: "Rory Gilmore",
+		photo: "https://media.allure.com/photos/580e7d382e7c1dc713a4171c/1:1/w_1600/rory6.jpg",
+		scores: [5, 4, 5, 3, 1, 1, 1, 5, 1, 5]
+	},
+	{
+		name: "Richard Gilmore",
+		photo: "https://vignette.wikia.nocookie.net/starshollow/images/3/38/Richard_Gilmore.jpg/revision/latest/scale-to-width-down/210?cb=20131029235307",
+		scores: [5, 1, 1, 5, 4, 2, 1, 1, 1, 1]
+	},
+	{
+		name: "Emily Gilmore",
+		photo: "http://images4.fanpop.com/image/photos/19400000/Emily-gilmore-girls-19462871-2068-2560.jpg",
+		scores: [5, 1, 1, 4, 5, 4, 1, 4, 2, 1]
+	},
+	{
+		name: "Sookie St. James",
+		photo: "https://assets.epicurious.com/photos/582b4de0b3446c1b212b9d64/2:1/w_1260%2Ch_630/why-gilmore-girls-fans-shouldnt-be-upset-about-sookies-absence.jpeg",
+		scores: [3, 2, 2, 1, 1, 5, 5, 3, 2, 3]
+	}
+];
+
+module.exports = friends;
